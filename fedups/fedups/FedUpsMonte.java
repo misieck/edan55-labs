385a12fef96967acb392166ef859d1a98a19fb92
Michal Pomorski <mi3031po-s@student.lu.se>
2024-05-10 19:07:04 +0200

    add accuracy measure

--- fedups/fedups/FedUpsMonte.java
+++ fedups/fedups/FedUpsMonte.java
@@ -1,7 +1,4 @@
-import java.util.Scanner;
-import java.util.TreeMap;
-import java.util.Random;
-import java.util.ArrayList;
+import java.util.*;
 import java.io.*;
 class Node {
     static Random r = new Random();
@@ -132,6 +129,11 @@ class DeliveryGraph {
 }
 
 public class FedUpsMonte {
+
+    public static double relativeAccuracy(double res, double exp){
+        return (exp - res)/exp;
+    }
+
     public static void main(String args[]) throws IOException {
 
         DeliveryGraph G = new DeliveryGraph(args[0]);
@@ -144,10 +146,12 @@ public class FedUpsMonte {
         Car cp = new Car(node[p], node[h]);
         node[h].scanGraph();
         if (node[f].accessible) {
+            
             for (int i = 0; i < 10000; i++) {
                 ftot += cf.deliver();
                 cf.reset();
             }
+
             System.out.println("Est. time FedUps = " + ftot / 10000);
         } else {
             System.out.println("FedUps: We tried to deliver your package, but you were not at home");
@@ -162,5 +166,87 @@ public class FedUpsMonte {
             System.out.println("PostNHL: We tried to deliver your package, but you were not at home");
         }
 
+
+        ftot = 0;
+        ptot = 0;
+        int REPS = 10000;
+        long start = System.nanoTime();
+        if (node[p].accessible && node[f].accessible ) {
+            double [] res = runSimulation(cf, cp, REPS);
+            ftot = res[0]; ptot = res[1];
+        }
+
+        long duration = System.nanoTime() - start;
+        Map<String, double[]> facits = new HashMap<>();
+        facits.put("toy", new double[]{18.27272727, 24.54545454 } );
+        facits.put("small", new double[]{233.69047619,233.333333333 } );
+
+        double fexp = facits.get(args[0])[0];
+        double pexp = facits.get(args[0])[1];
+        double fzeros = 0, fuck = 0;
+        double pzeros = 0, puck = 0;
+
+
+        presentResults(ftot, fexp, duration);
+        presentResults(ptot, pexp, duration);
+
+        if (node[p].accessible && node[f].accessible ) {
+            boolean stable = false;
+            while (!stable) {
+                stable = true;
+                for (int stableTest = 0; stableTest < 10; stableTest++) {
+                    start = System.nanoTime();
+                    double[] res = runSimulation(cf, cp, REPS);
+                    duration = System.nanoTime() - start;
+                    ftot = res[0];
+                    ptot = res[1];
+                    fzeros = zeros(ftot, fexp)[2];
+                    pzeros = zeros(ptot, pexp)[2];
+                    fuck = relativeAccuracy(ftot, fexp);
+                    puck = relativeAccuracy(ptot, pexp);
+                    if (fzeros < 2.5 || pzeros < 2.5) {
+                        stable = false;
+                    }
+                }
+                REPS *=2;
+                System.out.println("Reps: " + REPS/2 + ", ftot: " + ftot + ", ptot: " + ptot + ", fzeros: "+ fzeros+ ", pzeros: "+ pzeros + ", fuck: " + fuck+ ", puck: " + puck);
+            }
+            System.out.println("Stable with: " + REPS/2 + ", in " + duration/1000000 + "ms");
+        }
+
+
+
+
 	}
+
+    static double[] runSimulation(Car cf, Car cp, int REPS){
+        double ftot = 0, ptot = 0;
+        for (int i = 0; i < REPS; i++) {
+            ftot += cf.deliver();
+            cf.reset();
+            ptot += cp.deliver();
+            cp.reset();
+        }
+        return new double[]{ftot/REPS, ptot/REPS};
+    }
+
+    static double[] zeros(double result, double facit) {
+        double fuck = relativeAccuracy(result, facit);
+        long bits = Double.doubleToRawLongBits(fuck);
+        long exp = ((bits >>> 52) & 0x7FFL) - 1023;
+        double zeros = (-exp) * Math.log10(2);
+        return new double[]{fuck, zeros, -Math.log10(Math.abs(fuck)) };
+    }
+
+    static void presentResults(double result, double facit, long duration){
+
+        double[] zeros = zeros(result, facit);
+
+        //long test =  Double.doubleToRawLongBits(8) & 0x7FF0000000000000L ;
+        //long test2 = (( Double.doubleToRawLongBits(0.03) >>>52) & 0x7FFL)  - 1023;
+
+
+
+        System.out.println("Results: " + result + ", fuck: "+ zeros[0] + ", zeros: " + zeros[1] + " zeros "+zeros[2]+ ", time: " + duration/1000000);
+    }
 }
