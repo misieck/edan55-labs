e04d27ad47382a43651608024df2267973d12560
Michal Pomorski <mi3031po-s@student.lu.se>
2024-05-10 19:07:29 +0200

    add iterative can reach

--- fedups/fedups/FedUpsMarkov.java
+++ fedups/fedups/FedUpsMarkov.java
@@ -1,8 +1,14 @@
 
 import java.io.*;
-import java.util.HashSet;
+import java.util.*;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
 public class FedUpsMarkov {
     public static void main(String args[]) throws IOException {
+
+
         DeliveryGraph G = new DeliveryGraph(args[0]);
         double[][]A = G.A;
         double[] b = G.b;
@@ -12,6 +18,9 @@ public class FedUpsMarkov {
         int n = b.length;
 
         HashSet<Integer> reachable = findReachable(A, h);
+        var reach = canReach(A,h);
+        //System.out.println(Arrays.toString(reach));
+        System.out.println(reach);
         System.out.println(reachable);
         for (int x = 0; x < n; x++) {
             if (!reachable.contains(x)) {
@@ -33,6 +42,35 @@ public class FedUpsMarkov {
         else System.out.println("PostNHL time: Unreachable");
 
 	}
+
+    public static double[][] transpose(double [][]matrix){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        double[][] res = new double[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = i + 1; j < n; j++) {
+                res[i][j] = matrix[j][i];
+                res[j][i] = matrix[i][j];;
+            }
+        }
+        return res;
+    }
+
+    public static Collection<Integer> canReach(double [][] A, int home ) {
+        HashSet<Integer> res = new HashSet<>(Arrays.asList(home));
+        Stack<Integer> toAdd = new Stack<>();  toAdd.push(home);
+        double [][] _A = transpose(A);
+        IntPredicate resContains = res::contains;
+        while(!toAdd.isEmpty()){
+            double row[] = _A[toAdd.pop()];
+            IntStream.range(0, row.length)
+                    .filter(rowidx -> row[rowidx]!=0)
+                    .filter( resContains.negate() )
+                    .peek(toAdd::push)
+                    .forEach(res::add);
+        }
+        return res;
+    }
 
     public static HashSet<Integer> findReachable(double[][] A, int added) {
         return findReachable(A, new HashSet<>(),added);
